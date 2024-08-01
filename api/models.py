69868ae148083a7488e66609d28be35e8b0a78f3
nazarbekdev <nqobulov571@gmail.com>
2024-08-01 14:48:34 +0500

    models

--- api/models.py
+++ api/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
 
+class UserLanguage(models.Model):
+    user_id = models.IntegerField()
+    lan = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.lan
+
+
 class UserData(models.Model):
     user_id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.first_name
+        return self.name
+
+
+class Order(models.Model):
+    user_id = models.IntegerField()
+    direction = models.CharField(max_length=100)
+    day = models.CharField(max_length=100)
+    hour = models.CharField(max_length=100)
+    passengers = models.IntegerField()
+    additional_service = models.CharField()
+    description = models.TextField()
+    location = models.CharField()
+
+    def __str__(self):
+        return str(self.user_id)
+
